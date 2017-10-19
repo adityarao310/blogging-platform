b633adc3365cc1d4126b3d97d0f70cf0c676104f
adityarao310 <hello@adityarao.name>
2017-10-19 19:00:55 +0530

    Setup django models
    - POST
    - publish method
    - First few posts in db

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/adityarao310/Documents/everyoneShouldCode/finalProjects/python-django-virtualenv/djangoSample/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
